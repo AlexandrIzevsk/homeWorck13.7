276ade073fe8cfd369aebb9f83f903466df111c1
AlexandrIzevsk <sasha-sarov-333@rambler.ru>
2025-05-02 00:46:04 +0400

    Homework: add configure the JSON-server

---
+++ webpack.prod.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'production',
+    devServer: {
+        static: './dist',
+        hot: false,
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            title: 'Development',
+            template: './src/index.pug',
+            filename: "index.html"
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        esModule: true,
+                    }
+                }, 'css-loader']
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true,
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'main.js',
+    }
+}
